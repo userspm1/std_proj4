575c93d8cf1b7dbe04585b3a816dbeccf3d9895a
userspm1 <aswin.softwarescriptyum@gmail.com>
2024-09-04 19:23:35 +0530

    order

--- src/controllers/orderController.js
+++ src/controllers/orderController.js
@@ -4,17 +4,40 @@ const express = require('express')
 
 
 exports.placeOrder = async (req, res) => {
-  const { orderId, orderDate, orderPerson } = req.body; // Product ID from URL 
+  const { orderId, orderDate, orderPerson,orderContact,orderAddress } = req.body; // Product ID from URL 
   try {
     // Find the product by ID and update it
-    const user = new Order({ orderId, orderDate, orderPerson })
+    const user = new Order({ orderId, orderDate, orderPerson,orderContact,orderAddress })
 
     await user.save();
 
     // Send success response
     res.status(200).json({
-      message: 'Product updated successfully',
+      message: 'Order Placed successfully',
       user
+    });
+  } catch (error) {
+    // Handle errors
+    console.error('Error updating product:', error);
+    res.status(400).json({
+      error: error.message
+    });
+  }
+};
+exports.updateOrder = async (req, res) => {
+  const {id} = req.params;
+  const  {orderStatus} = req.body; // Product ID from URL 
+  try {
+    // Find the product by ID and update it
+    const userStatus = await  Order.findByIdAndUpdate(id, {orderStatus},{
+      new: true, // Return the updated document 
+      runValidators: true 
+    })
+ 
+    // Send success response
+    res.status(200).json({
+      message: 'Order updated successfully',
+      userStatus
     });
   } catch (error) {
     // Handle errors
--- src/routes/orderRoute.js
+++ src/routes/orderRoute.js
@@ -1,8 +1,9 @@
 const express = require('express'); 
-const { placeOrder, getOrder } = require('../controllers/orderController');
+const { placeOrder, getOrder, updateOrder } = require('../controllers/orderController');
 const router = express.Router();
 
 
 router.post('/order', placeOrder); 
 router.get('/get/order', getOrder); 
+router.put('/update/order/:id', updateOrder); 
 module.exports = router
